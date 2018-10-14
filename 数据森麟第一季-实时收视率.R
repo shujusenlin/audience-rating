82a8c620c6573f7f2ff7d660d11b4d597bab0c30
shujusenlin <34412690+shujusenlin@users.noreply.github.com>
2018-10-14 18:57:13 +0800

    Update 数据森麟第一季-实时收视率.R

--- 数据森麟第一季-实时收视率.R
+++ 数据森麟第一季-实时收视率.R
@@ -1,12 +1,23 @@
+
 ##########PART1：读取必要package########## 
+## 字符处理
 library(stringr)
+## 数据爬取
 library(RCurl)
 library(XML)
 library(RSelenium)
+## 数据处理
 library(sqldf)
-library(maptools)   
+library(data.table)
+library(dplyr)
+## 地图文件处理
+library(maptools)  
+library(mapproj)
+## 数据可视化
 library(ggplot2)    
 library(ggthemes)
+
+
 
 ##########PART2：开始爬取过程########## 
 setwd('D:/爬虫/收视率')
@@ -18,64 +29,27 @@ remDr <- remoteDriver(remoteServerAddr = "127.0.0.1"
 
 ## 打开浏览器
 remDr$open()
-remDr$navigate('http://www.csm-huan.com/index_nologin.html')
-tv_rate <- c()
+remDr$navigate('http://www.csm-huan.com/index_weishi.html')
+tv_rate <- data.frame(num=1:33)
 
-## 循环爬取48个电视台的收视率
-i = 1
-while(i <= 48){
-  station <- NA
-  program <- NA
-  rate <- NA
-  tryNext <- try(
-    {## 获得电视台名称
-     ele_str1 = sprintf('//*[@id="tbody"]/tr[%d]/td[1]/a',i)
-     elem_1 <- getNodeSet(htmlParse(remDr$findElement(using = "xpath",
-                          ele_str1)$getElementAttribute("outerHTML")[[1]],encoding='utf-8'),
-                          '//a[@href="javascript:void(0);"]')
-     station <- sapply(elem_1,xmlValue)
-     station <- strsplit(station,split='\\.')[[1]][2]
-     ## 获得节目名称
-     ele_str2 = sprintf('//*[@id="tbody"]/tr[%d]/td[2]/a',i)
-     elem_2 <- getNodeSet(htmlParse(remDr$findElement(using = "xpath",
-                          ele_str2)$getElementAttribute("outerHTML")[[1]],encoding='utf-8'),
-                          '//a[@href="javascript:void(0);"]')
-     program <- sapply(elem_2,xmlValue)
-     ## 获得收视率数值(没有上升/下降符号的数值)
-     ele_str3 = sprintf('//*[@id="tbody"]/tr[%d]/td[3]/a',i)
-     elem_3_1 <- getNodeSet(htmlParse(remDr$findElement(using = "xpath",
-                            ele_str3)$getElementAttribute("outerHTML")[[1]],encoding='utf-8'),
-                            '//span[@class=""]')
-     rate_1<- sapply(elem_3_1,xmlValue)
-     ## 获得收视率数值(带有上升符号的数值)
-     elem_3_2 <- getNodeSet(htmlParse(remDr$findElement(using = "xpath",
-                            ele_str3)$getElementAttribute("outerHTML")[[1]],encoding='utf-8'),
-                            '//span[@class="up"]')
-     rate_2<- sapply(elem_3_2,xmlValue)
-     ## 获得收视率数值(带有下降符号的数值)
-     elem_3_3 <- getNodeSet(htmlParse(remDr$findElement(using = "xpath",
-                            ele_str3)$getElementAttribute("outerHTML")[[1]],encoding='utf-8'),
-                            '//span[@class="down"]')
-     rate_3<- sapply(elem_3_3,xmlValue)
-     ## 整合收视率数值
-     if(length(rate_1)>0){rate = rate_1}
-     if(length(rate_2)>0){rate = rate_2}
-     if(length(rate_3)>0){rate = rate_3}
-     tv_rate <- rbind(tv_rate,c(station,program,rate))
-     }
-  )
-  ## 确认是否成功爬取
-  print(i)
-  if (is.na(rate)) {i = i-1}
-  i = i+1
+## 循环爬取33个电视台的收视率
+tv_rate$station <- 0
+tv_rate$program <- 0
+tv_rate$rate <- 0
+tbody = '//*[@id="tbody"]'
+content  <- getNodeSet(htmlParse(remDr$findElement(using = "xpath",
+          tbody)$getElementAttribute("outerHTML")[[1]],encoding='utf-8'),
+                     '//td')
+content <- sapply(content,xmlValue)
+for(i in 1:33){
+  tv_rate$station[i]=gsub('\\d','',content[i*3-2])
+  tv_rate$program[i]=content[i*3-1]
+  tv_rate$rate[i]=as.numeric(gsub('\\%','',content[i*3]))
 }
-remDr$close()
+
 
 ##########PART3：实时收视率写入本地########## 
-tv_rate <- as.data.frame(tv_rate)
-colnames(tv_rate) <- c('station','programe','rate')
-tv_rate$time <- Sys.time()
-loc <- sprintf('实时收视率%s.csv',format(Sys.time(),format="%Y_%m_%d_%H_%M"))
+loc <- sprintf('实时收视率%s.csv',format(Sys.time(),format="%m%d%H%M"))
 write.csv(tv_rate,loc)
 
 ##########PART4：地图文件绘制########## 
@@ -94,13 +68,12 @@ for(i in 1:nrow(province_rate)){
   this_program <- strsplit(this_program,split='\\(')
   province_rate$program_name[i] <- this_program[[1]][1]
 }
-province_rate$rank <- as.numeric(1:31)
-for(i in 1:9){province_rate$rank[i] <- paste(0,i,sep='')}
-province_rate$com <- paste(province_rate$rank,'  ',province_rate$station,':',
+province_rate$com <- paste(province_rate$station,':',
                            as.character(province_rate$rate),'%',sep = '')
+province_rate$rank <-1:31
 
 ## 地图文件融合
-china_map <- readShapePoly("中国地图shp格式/china_basic_map/bou2_4p.shp")
+china_map <- readShapePoly("bou2_4p.shp")
 china_map1 <- china_map@data
 china_map1$id <- 0:(nrow(china_map1)-1)
 china_map1$id <- as.character(china_map1$id)
@@ -126,7 +99,7 @@ ggplot() +
                     axis.title = element_blank(),
                     legend.position = 'NONE',
                     plot.title = element_text(hjust=0.5,size=25)
-                    )
+  )
 
 
 ## 分省情况绘制
@@ -150,6 +123,16 @@ ggplot() +
   facet_wrap(~com,scales = 'free',shrink = TRUE)+
   geom_text(data=subset(province_rate,rank!=31),aes(x=longitude,
             y=latitude,label=program_name),size=2.8,alpha=0.8)
+
+
+
+
+
+
+
+
+
+
 
 
 
